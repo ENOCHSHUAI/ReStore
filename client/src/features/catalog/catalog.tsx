855a3e1435fa7e95aa933551d50a75efaa9fb154
ENOCHSHUAI <ysun344@aucklanduni.ac.nz>
2024-07-09 00:34:42 +1200

    try

---
+++ client/src/features/catalog/catalog.tsx
@@ -0,0 +1,18 @@
+interface Props{
+    products: Product[];
+    addProduct:()=>void;
+}
+
+
+export default function Catalog(props: Props){
+    return (
+        <>
+            <ul>
+                {props.products.map(product => (
+                    <li key={product.id}>{product.name} - {product.price}</li>
+                ))}
+            </ul>
+            <button onClick={props.addProduct}>Add product</button>
+        </>
+    )
+}
